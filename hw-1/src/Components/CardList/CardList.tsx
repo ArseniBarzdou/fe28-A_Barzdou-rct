06160578259d466fc30f5a0dd8a9edb35b1ce3c3
Arseni Barzdou <arseniy9898@gmail.com>
2022-09-21 00:03:30 +0300

    update style card

--- hw-1/src/Components/CardList/CardList.tsx
+++ hw-1/src/Components/CardList/CardList.tsx
@@ -20,7 +20,7 @@ const CardList = () => {
         {
             id: 1,
             image:
-            "https://cdn.pixabay.com/photo/2017/08/30/01/05/milky-way-2695569_1280.jpg",
+            "https://cdn.pixabay.com/photo/2011/12/13/14/30/earth-11014_960_720.jpg",
             text: "Astronauts Kayla Barron and Raja Chari floated out of the International Space Station airlock for a spacewalk Tuesday, installing brackets and struts to support new solar arrays to upgrade the research lab’s power system on the same day that crewmate Mark Vande Hei marked his 341st day in orbit, a U.S. record for a single spaceflight.",
             date: "April 20, 2021",
             lesson_num: 0,
@@ -31,7 +31,7 @@ const CardList = () => {
         {
             id: 2,
             image:
-            "https://cdn.pixabay.com/photo/2017/08/30/01/05/milky-way-2695569_1280.jpg",
+            "https://cdn.pixabay.com/photo/2012/10/10/11/05/space-station-60615_960_720.jpg",
             text: "Astronauts Kayla Barron and Raja Chari floated out of the International Space Station airlock for a spacewalk Tuesday, installing brackets and struts to support new solar arrays to upgrade the research lab’s power system on the same day that crewmate Mark Vande Hei marked his 341st day in orbit, a U.S. record for a single spaceflight.",
             date: "April 20, 2021",
             lesson_num: 0,
@@ -42,7 +42,7 @@ const CardList = () => {
         {
             id: 3,
             image:
-            "https://cdn.pixabay.com/photo/2017/08/30/01/05/milky-way-2695569_1280.jpg",
+            "https://cdn.pixabay.com/photo/2011/12/13/17/09/astronaut-11050_960_720.jpg",
             text: "Astronauts Kayla Barron and Raja Chari floated out of the International Space Station airlock for a spacewalk Tuesday, installing brackets and struts to support new solar arrays to upgrade the research lab’s power system on the same day that crewmate Mark Vande Hei marked his 341st day in orbit, a U.S. record for a single spaceflight.",
             date: "April 20, 2021",
             lesson_num: 0,
@@ -53,7 +53,7 @@ const CardList = () => {
         {
             id: 4,
             image:
-            "https://cdn.pixabay.com/photo/2017/08/30/01/05/milky-way-2695569_1280.jpg",
+            "https://cdn.pixabay.com/photo/2013/03/02/02/20/space-89130_960_720.jpg",
             text: "Astronauts Kayla Barron and Raja Chari floated out of the International Space Station airlock for a spacewalk Tuesday, installing brackets and struts to support new solar arrays to upgrade the research lab’s power system on the same day that crewmate Mark Vande Hei marked his 341st day in orbit, a U.S. record for a single spaceflight.",
             date: "April 20, 2021",
             lesson_num: 0,
@@ -64,7 +64,7 @@ const CardList = () => {
         {
             id: 5,
             image:
-            "https://cdn.pixabay.com/photo/2017/08/30/01/05/milky-way-2695569_1280.jpg",
+            "https://cdn.pixabay.com/photo/2018/03/29/19/34/northern-lights-3273425_960_720.jpg",
             text: "Astronauts Kayla Barron and Raja Chari floated out of the International Space Station airlock for a spacewalk Tuesday, installing brackets and struts to support new solar arrays to upgrade the research lab’s power system on the same day that crewmate Mark Vande Hei marked his 341st day in orbit, a U.S. record for a single spaceflight.",
             date: "April 20, 2021",
             lesson_num: 0,
@@ -75,7 +75,7 @@ const CardList = () => {
         {
             id: 6,
             image:
-            "https://cdn.pixabay.com/photo/2017/08/30/01/05/milky-way-2695569_1280.jpg",
+            "https://cdn.pixabay.com/photo/2011/12/13/14/30/earth-11014_960_720.jpg",
             text: "Astronauts Kayla Barron and Raja Chari floated out of the International Space Station airlock for a spacewalk Tuesday, installing brackets and struts to support new solar arrays to upgrade the research lab’s power system on the same day that crewmate Mark Vande Hei marked his 341st day in orbit, a U.S. record for a single spaceflight.",
             date: "April 20, 2021",
             lesson_num: 0,
@@ -86,7 +86,7 @@ const CardList = () => {
         {
             id: 7,
             image:
-            "https://cdn.pixabay.com/photo/2017/08/30/01/05/milky-way-2695569_1280.jpg",
+            "https://cdn.pixabay.com/photo/2018/03/29/19/34/northern-lights-3273425_960_720.jpg",
             text: "Astronauts Kayla Barron and Raja Chari floated out of the International Space Station airlock for a spacewalk Tuesday, installing brackets and struts to support new solar arrays to upgrade the research lab’s power system on the same day that crewmate Mark Vande Hei marked his 341st day in orbit, a U.S. record for a single spaceflight.",
             date: "April 20, 2021",
             lesson_num: 0,
@@ -97,7 +97,7 @@ const CardList = () => {
         {
             id: 8,
             image:
-            "https://cdn.pixabay.com/photo/2017/08/30/01/05/milky-way-2695569_1280.jpg",
+            "https://cdn.pixabay.com/photo/2013/03/02/02/20/space-89130_960_720.jpg",
             text: "Astronauts Kayla Barron and Raja Chari floated out of the International Space Station airlock for a spacewalk Tuesday, installing brackets and struts to support new solar arrays to upgrade the research lab’s power system on the same day that crewmate Mark Vande Hei marked his 341st day in orbit, a U.S. record for a single spaceflight.",
             date: "April 20, 2021",
             lesson_num: 0,
@@ -108,7 +108,7 @@ const CardList = () => {
         {
             id: 9,
             image:
-            "https://cdn.pixabay.com/photo/2017/08/30/01/05/milky-way-2695569_1280.jpg",
+            "https://cdn.pixabay.com/photo/2018/03/29/19/34/northern-lights-3273425_960_720.jpg",
             text: "Astronauts Kayla Barron and Raja Chari floated out of the International Space Station airlock for a spacewalk Tuesday, installing brackets and struts to support new solar arrays to upgrade the research lab’s power system on the same day that crewmate Mark Vande Hei marked his 341st day in orbit, a U.S. record for a single spaceflight.",
             date: "April 20, 2021",
             lesson_num: 0,
@@ -119,7 +119,7 @@ const CardList = () => {
         {
             id: 10,
             image:
-                "https://cdn.pixabay.com/photo/2017/08/30/01/05/milky-way-2695569_1280.jpg",
+            "https://cdn.pixabay.com/photo/2011/12/13/17/09/astronaut-11050_960_720.jpg",
             text: "Astronauts Kayla Barron and Raja Chari floated out of the International Space Station airlock for a spacewalk Tuesday, installing brackets and struts to support new solar arrays to upgrade the research lab’s power system on the same day that crewmate Mark Vande Hei marked his 341st day in orbit, a U.S. record for a single spaceflight.",
             date: "April 20, 2021",
             lesson_num: 0,
@@ -130,7 +130,7 @@ const CardList = () => {
         {
             id: 11,
             image:
-            "https://cdn.pixabay.com/photo/2017/08/30/01/05/milky-way-2695569_1280.jpg",
+            "https://cdn.pixabay.com/photo/2012/10/10/11/05/space-station-60615_960_720.jpg",
             text: "Astronauts Kayla Barron and Raja Chari floated out of the International Space Station airlock for a spacewalk Tuesday, installing brackets and struts to support new solar arrays to upgrade the research lab’s power system on the same day that crewmate Mark Vande Hei marked his 341st day in orbit, a U.S. record for a single spaceflight.",
             date: "April 20, 2021",
             lesson_num: 0,
